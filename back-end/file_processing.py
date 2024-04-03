3d7ca9a38bef1812bfb953c94570a7fdcf4d1e82
Ekkeskit <126162842+Ekkeskit@users.noreply.github.com>
2024-04-03 17:47:42 +0200

    more changes

--- back-end/file_processing.py
+++ back-end/file_processing.py
@@ -18,9 +18,10 @@ PDF_PATH = "./PDF"
 MD_PATH = "./MD"
 PROCESSED_PATH = "./processed"
 
-# files
-CSV_FILE = os.path.join(UPLOADS_PATH, "people.csv")
-MARKDOWN_TEMPLATE = os.path.join(UPLOADS_PATH, "template.md")
+# files in variables. Will define them when processing files
+CSV_FILE = None
+MARKDOWN_TEMPLATE = None
+
 
 
 # home, just renders the form page
@@ -32,13 +33,20 @@ def home():
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
-        uploaded_file = request.files['file']
-        if uploaded_file.filename != '':
-            file_path = os.path.join(UPLOADS_PATH, uploaded_file.filename)
-            uploaded_file.save(file_path)
-            process_files(file_path)
-            # make download link available
-            return render_template('upload.html', download=True)
+        if 'file' not in request.files:
+            return redirect(request.url)
+        
+        uploaded_files = request.files.getlist('file')
+
+        for uploaded_file in uploaded_files:
+            if uploaded_file.filename != '':
+                file_path = os.path.join(UPLOADS_PATH, uploaded_file.filename)
+                uploaded_file.save(file_path)
+                process_files(file_path)
+
+        # make download link available
+        return render_template('upload.html', download=True)
+            
     return render_template('upload.html')
 
 @app.route('/download')
@@ -54,21 +62,43 @@ def extract_gz(file_path):
 
 
 def process_files(file_path):
-    # extract .tar.gz file
     if file_path.endswith('.tar.gz'):
         extract_gz(file_path)
         os.remove(file_path)
+        process_extracted_files()
+    elif file_path.endswith('.csv'):
+        global CSV_FILE
+        CSV_FILE = file_path
+    elif file_path.endswith('.md'):
+        global MARKDOWN_TEMPLATE
+        MARKDOWN_TEMPLATE = file_path
 
-    # file processing logic
-    csv_data = read_csv_file()
-    modify_and_write_markdown(csv_data)
+    csv_data = None
+    if CSV_FILE and MARKDOWN_TEMPLATE:
+        csv_data = read_csv_file()
+        modify_and_write_markdown(csv_data)
+        create_pdfs()
+        create_tar_file()
+    else:
+        print("CSV file and Markdown template not found. Cannot proceed with processing.")
 
+def process_extracted_files():
+    for extracted_file in os.listdir(UPLOADS_PATH):
+        extracted_file_path = os.path.join(UPLOADS_PATH, extracted_file)
+        if extracted_file.endswith('.csv'):
+            global CSV_FILE
+            CSV_FILE = extracted_file_path
+        elif extracted_file.endswith('.md'):
+            global MARKDOWN_TEMPLATE
+            MARKDOWN_TEMPLATE = extracted_file_path
+
+def create_pdfs():
     for mdfile in os.listdir(MD_PATH):
         if mdfile.endswith(".md"):
             md_file_path = os.path.join(MD_PATH, mdfile)
             convert_to_pdf(md_file_path)
 
-    # make .tar.gz file with certificates
+def create_tar_file():
     with tarfile.open(os.path.join(PROCESSED_PATH, 'certificates.tar.gz'), "w:gz") as tar:
         tar.add(PDF_PATH, arcname=os.path.basename(PDF_PATH))
 
