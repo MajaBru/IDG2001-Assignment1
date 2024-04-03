5c8d21c2784830fbe289a9376337f24cab8077d6
sabrina <sabrina130102@gmail.com>
2024-04-03 17:25:25 +0200

    testing

---
+++ back-end/MD/Jamie_Smith.md
@@ -0,0 +1,8 @@
+# Certificate of excellence
+![NTNU-logo](../files/NTNU-logo.png)
+Jamie Smith have completed the course IDG2001 Cloud Technologies at
+(NTNU). As part of their course, they have blabla skills, lists, etc.
+- SaaS, PaaS, IaaS
+- Cloud infrastructure ... etc
+![Signature](signature.png)
+Paul Knutson, Faculty of IE, NTNU
--- front-end/download.js
+++ front-end/download.js
@@ -1,24 +1,24 @@
 
-/* const submitButton = document.querySelector("#upload-btn");
+// const submitButton = document.querySelector("#upload-btn");
 
-document.querySelector("#upload-form").addEventListener('submit', function(e) {
-    e.preventDefault(); */
+// document.querySelector("#upload-form").addEventListener('submit', function(e) {
+//     e.preventDefault(); 
 
-    /* let downloadLink = document.querySelector('#download-link'); */
+//     let downloadLink = document.querySelector('#download-link'); 
     
-        /* fetch('/download', {
-            method: 'GET',  
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            window.location.href = '/download';
-            return response.json();
-        })
-        .catch(error => {
-            console.error('Error processing and downloading file:', error);
-        });
-    });  */// Delay in milliseconds, adjust as needed
+//        fetch('/download', {
+//             method: 'GET',  
+//         })
+//         .then(response => {
+//             if (!response.ok) {
+//                 throw new Error('Network response was not ok');
+//             }
+//             window.location.href = '/download';
+//             return response.json();
+//         })
+//         .catch(error => {
+//             console.error('Error processing and downloading file:', error);
+//         });
+//     });  /// Delay in milliseconds, adjust as needed
 
     
