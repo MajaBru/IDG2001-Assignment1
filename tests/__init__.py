4bda38821e0650a62a1a9404745658b41fb82ae9
Ekkeskit <126162842+Ekkeskit@users.noreply.github.com>
2024-04-07 13:17:33 +0200

    changes

--- tests/__init__.py
+++
@@ -1,5 +0,0 @@
-# The __init__.py file makes Python treat directories containing the file as packages.
-#This prevents directories with a common name, such as string,
-#unintentionally hiding valid modules that occur later on the module search path.
-#In the simplest case, __init__.py can just be an empty file,
-#but it can also execute initialization code for the package or set the __all__ variable, described later.
--- tests/test_file_processing.py
+++
@@ -1,1 +0,0 @@
-#Testing for file processing goes here
